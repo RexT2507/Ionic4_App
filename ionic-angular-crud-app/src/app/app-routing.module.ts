2fe624e371dce295267bb37cdc8866c750c85e63
RexT2507 <baptiste.marmier@ynov.com>
2020-02-03 15:41:11 +0100

    [ADD]  Configuration des routes Angular 8 dans l'application Ionic 4

--- ionic-angular-crud-app/src/app/app-routing.module.ts
+++ ionic-angular-crud-app/src/app/app-routing.module.ts
@@ -4,6 +4,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'add-song',
+    loadChildren: () => import('./add-song/add-song.module').then( m => m.AddSongPageModule)
+  },
+  {
+    path: 'edit-song/:id',
+    loadChildren: () => import('./edit-song/edit-song.module').then( m => m.EditSongPageModule)
+  },
 ];
 
 @NgModule({
