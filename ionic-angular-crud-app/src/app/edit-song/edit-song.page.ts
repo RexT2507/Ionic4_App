18177fb1be90de6fdb6b0b9f15c293ba117d878a
RexT2507 <baptiste.marmier@ynov.com>
2020-02-10 16:28:18 +0100

    [DEV] Mise en place et test de la page update et test

--- ionic-angular-crud-app/src/app/edit-song/edit-song.page.ts
+++ ionic-angular-crud-app/src/app/edit-song/edit-song.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { SongService } from '../service/song.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-song',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditSongPage implements OnInit {
 
-  constructor() { }
+  updateSongForm: FormGroup;
+  id: any;
+
+  constructor(
+    private songAPI: SongService,
+    private actRoute: ActivatedRoute,
+    private router: Router,
+    public fb: FormBuilder
+  ) {
+    this.id = this.actRoute.snapshot.paramMap.get('id');
+  }
 
   ngOnInit() {
+    this.getSongData(this.id);
+    this.updateSongForm = this.fb.group({
+      song_name: [''],
+      artist: ['']
+    });
   }
+
+  getSongData(id) {
+    this.songAPI.getSong(id).subscribe(res => {
+      this.updateSongForm.setValue({
+        song_name: res['song_name'],
+        artist: res['artist']
+      });
+    });
+  }
+
+
+  updateForm() {
+    if (!this.updateSongForm.valid) {
+      return false;
+    } else {
+      this.songAPI.updateSong(this.id, this.updateSongForm.value)
+        .subscribe((res) => {
+          console.log(res);
+          this.updateSongForm.reset();
+          this.router.navigate(['/home']);
+        });
+    }
+  }
+
 
 }
